ce88d1017281563ac0f3de318397de504ce57c37
jkokorian <jkokorian@gmail.com>
2015-05-25 13:50:21 +0200

    manifest added, published 0.1 to pypi

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from setuptools import setup
 
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
 setup(name='pyqt2waybinding',
       version='0.1',
       description='Simple framework for easy 2-way binding with PyQt',
+      long_description=readme(),
       url='http://github.com/jkokorian/pyqt2waybinding',
       author='J. Kokorian',
       author_email='J.Kokorian@TUDelft.nl',
@@ -13,4 +18,5 @@ setup(name='pyqt2waybinding',
       install_requires=[],
       test_suite='nose.collector',
       tests_require=['nose'],
+      include_package_data=True,
       zip_safe=False)
